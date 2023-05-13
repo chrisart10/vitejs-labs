331a3268d6d867954c544e83e0924287d92ff13d
Christian Espinoza <christian.espinoza1008@gmail.com>
2023-05-13 14:56:36 -0500

    +- Input no focus issue

--- src/pages/lab1/Lab1.jsx
+++ src/pages/lab1/Lab1.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 import FormList from "./FormList";
 
 function Lab1() {
+	const [formData, setFormData] = useState({ firstName: "", lastName: "" });
 	const [InputList] = FormList({
 		onChange: (data) => {
 			console.log("data modificada", data);
+			setFormData((prev) => ({ ...prev, ...data }));
 		},
 	});
 	console.log("-----------------------LAB1-----------------------");
@@ -14,6 +18,9 @@ function Lab1() {
 				<input type="text" name="firstName" />
 				<input type="text" name="lastName" />
 			</InputList>
+			<p>
+				{formData.firstName} {formData.lastName}
+			</p>
 		</div>
 	);
 }
